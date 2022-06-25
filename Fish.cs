18f88b1a7d913d7c284835921c3c55fd6acf5f90
BulatTim <32904838+BulatTim1@users.noreply.github.com>
2022-06-25 09:08:49 +0300

    Add project files.

---
+++ Fish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_test
+{
+    internal class Fish
+    {
+        public string? Name { get; set; }
+        public int MaxTemp { get; set; }
+        public int MinTemp  { get; set; }
+        public int MaxTempTime { get; set; }
+        public int MinTempTime { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace csharp_test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<(DateTime Date, int ActualValue, int NormalValue, int Offset)> report;
+        public MainWindow()
+        {
+            InitializeComponent();
+            report = new List<(DateTime Date, int ActualValue, 
+                int NormalValue, int Offset)>();
+        }
+
+        private void FormClear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FormSaveReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FormLoadData_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FormMakeReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
